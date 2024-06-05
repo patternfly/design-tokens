7163900eb0d9b21cb32315bb379f4bfbd6de528a
Sarah Rambacher <srambach@redhat.com>
2024-06-05 09:30:55 -0400

    feat(exporter): export description to json files

--- packages/module/plugins/export-patternfly-tokens/code.js
+++ packages/module/plugins/export-patternfly-tokens/code.js
@@ -43,7 +43,7 @@ function processCollection({ name, modes, variableIds }) {
     let file = { fileName: `${name}.${mode.name}.tokens.json`, body: {} };
 
     variableIds.forEach((variableId) => {
-      const { name, resolvedType, valuesByMode } =
+      const { name, resolvedType, valuesByMode, description } =
         figma.variables.getVariableById(variableId);
       const value = valuesByMode[mode.modeId];
 
@@ -53,8 +53,11 @@ function processCollection({ name, modes, variableIds }) {
           obj[groupName] = obj[groupName] || {};
           obj = obj[groupName];
         });
-    
-    
+
+        if (description) {
+          obj.description = description;
+        }
+
         if (value.type === "VARIABLE_ALIAS") {
           obj.$type = resolvedType === "COLOR" ? "color" : "number";
           obj.$value = `{${figma.variables
