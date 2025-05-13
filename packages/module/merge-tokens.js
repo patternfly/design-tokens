29288bc1812a3b3c22deaa610346580dce7a6753
Sarah Rambacher <srambach@redhat.com>
2025-05-13 09:39:20 -0400

    feat: output separate merged token files for default and dark themes

--- packages/module/merge-tokens.js
+++ packages/module/merge-tokens.js
@@ -1,23 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 const { merge } = require('lodash');
-
 const glob = require('glob');
 
-// Find all JSON token files in default and dark themes
-const tokenFiles = [
-  ...glob.sync(path.join(__dirname, 'tokens/default/*.json')),
-  ...glob.sync(path.join(__dirname, 'tokens/dark/*.json')),
-];
+function mergeTokensForTheme(theme) {
+  const tokenFiles = glob.sync(path.join(__dirname, `tokens/${theme}/*.json`));
+  let mergedTokens = {};
+  tokenFiles.forEach(file => {
+    const content = JSON.parse(fs.readFileSync(file, 'utf8'));
+    mergedTokens = merge(mergedTokens, content);
+  });
+  return mergedTokens;
+}
 
-let mergedTokens = {};
+const mergedDefault = mergeTokensForTheme('default');
+const mergedDark = mergeTokensForTheme('dark');
 
-tokenFiles.forEach(file => {
-  const content = JSON.parse(fs.readFileSync(file, 'utf8'));
-  mergedTokens = merge(mergedTokens, content);
-});
+const outputDefault = path.join(__dirname, 'tokens/merged-default-tokens.json');
+const outputDark = path.join(__dirname, 'tokens/merged-dark-tokens.json');
 
-const outputPath = path.join(__dirname, 'tokens/merged-tokens.json');
-fs.writeFileSync(outputPath, JSON.stringify(mergedTokens, null, 2));
+fs.writeFileSync(outputDefault, JSON.stringify(mergedDefault, null, 2));
+fs.writeFileSync(outputDark, JSON.stringify(mergedDark, null, 2));
 
-console.log(`Merged tokens written to ${outputPath}`); 
+console.log(`Merged default tokens written to ${outputDefault}`);
+console.log(`Merged dark tokens written to ${outputDark}`); 
