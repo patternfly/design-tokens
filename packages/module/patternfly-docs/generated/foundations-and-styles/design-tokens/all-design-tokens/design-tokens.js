ed4c26540787187a7516b78bc727be7c52662d2a
Tarun Vashishtha <tarunvashishtha@Taruns-MacBook-Air.local>
2026-02-01 21:26:35 +0530

    chore: regenerate token outputs for gradient token support
    Regenerate all SCSS token files and documentation after tokens were
    updated in Figma to include gradient support for skeleton loading states.
    
    - Regenerate SCSS token files across all themes
    - Regenerate documentation with updated token layers:
      - Default, Dark, Glass, Glass-Dark
      - HighContrast, HighContrast-Dark
      - RedHat, RedHat-Dark, RedHat-Glass, RedHat-Glass-Dark
      - RedHat-HighContrast, RedHat-HighContrast-Dark
    - Update generated documentation components

--- packages/module/patternfly-docs/generated/foundations-and-styles/design-tokens/all-design-tokens/design-tokens.js
+++ packages/module/patternfly-docs/generated/foundations-and-styles/design-tokens/all-design-tokens/design-tokens.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { AutoLinkHeader, Example, Link as PatternflyThemeLink } from '@patternfly/documentation-framework/components';
 import * as defaultTokens from '../../../../content/./token-layers-default.json';
 import * as darkTokens from '../../../../content/./token-layers-dark.json';
+import * as glassTokens from '../../../../content/./token-layers-glass.json';
+import * as glassDarkTokens from '../../../../content/./token-layers-glass-dark.json';
+import * as highcontrastTokens from '../../../../content/./token-layers-highcontrast.json';
+import * as highcontrastDarkTokens from '../../../../content/./token-layers-highcontrast-dark.json';
+import * as redhatTokens from '../../../../content/./token-layers-redhat.json';
+import * as redhatDarkTokens from '../../../../content/./token-layers-redhat-dark.json';
+import * as redhatGlassTokens from '../../../../content/./token-layers-redhat-glass.json';
+import * as redhatGlassDarkTokens from '../../../../content/./token-layers-redhat-glass-dark.json';
+import * as redhatHighcontrastTokens from '../../../../content/./token-layers-redhat-highcontrast.json';
+import * as redhatHighcontrastDarkTokens from '../../../../content/./token-layers-redhat-highcontrast-dark.json';
 import { TokensTable } from '../../../../content/./tokensTable.js';
 const pageData = {
   "id": "All design tokens",
@@ -16,21 +26,45 @@ const pageData = {
   "tabName": null,
   "slug": "/foundations-and-styles/design-tokens/all-design-tokens/design-tokens",
   "sourceLink": "https://github.com/patternfly/patternfly-org/blob/main/packages/module/patternfly-docs/content/all-patternfly-tokens.md",
-  "relPath": "packages/module/patternfly-docs/content/all-patternfly-tokens.md"
+  "relPath": "packages/module/patternfly-docs/content/all-patternfly-tokens.md",
+  "sortValue": 5
 };
 pageData.liveContext = {
   defaultTokens,
   darkTokens,
+  glassTokens,
+  glassDarkTokens,
+  highcontrastTokens,
+  highcontrastDarkTokens,
+  redhatTokens,
+  redhatDarkTokens,
+  redhatGlassTokens,
+  redhatGlassDarkTokens,
+  redhatHighcontrastTokens,
+  redhatHighcontrastDarkTokens,
   TokensTable
 };
-pageData.relativeImports = "import * as defaultTokens from 'content/./token-layers-default.json';,import * as darkTokens from 'content/./token-layers-dark.json';,import { TokensTable } from 'content/./tokensTable.js';"
+pageData.relativeImports = "import * as defaultTokens from 'content/./token-layers-default.json';,import * as darkTokens from 'content/./token-layers-dark.json';,import * as glassTokens from 'content/./token-layers-glass.json';,import * as glassDarkTokens from 'content/./token-layers-glass-dark.json';,import * as highcontrastTokens from 'content/./token-layers-highcontrast.json';,import * as highcontrastDarkTokens from 'content/./token-layers-highcontrast-dark.json';,import * as redhatTokens from 'content/./token-layers-redhat.json';,import * as redhatDarkTokens from 'content/./token-layers-redhat-dark.json';,import * as redhatGlassTokens from 'content/./token-layers-redhat-glass.json';,import * as redhatGlassDarkTokens from 'content/./token-layers-redhat-glass-dark.json';,import * as redhatHighcontrastTokens from 'content/./token-layers-redhat-highcontrast.json';,import * as redhatHighcontrastDarkTokens from 'content/./token-layers-redhat-highcontrast-dark.json';,import { TokensTable } from 'content/./tokensTable.js';"
 pageData.examples = {
   
 };
 
 const Component = () => (
   <React.Fragment>
-    <TokensTable tokenJson={{default: defaultTokens, dark: darkTokens}}/>
+    <TokensTable tokenJson={{
+    default: defaultTokens,
+    dark: darkTokens,
+    glass: glassTokens,
+    'glass-dark': glassDarkTokens,
+    highcontrast: highcontrastTokens,
+    'highcontrast-dark': highcontrastDarkTokens,
+    redhat: redhatTokens,
+    'redhat-dark': redhatDarkTokens,
+    'redhat-glass': redhatGlassTokens,
+    'redhat-glass-dark': redhatGlassDarkTokens,
+    'redhat-highcontrast': redhatHighcontrastTokens,
+    'redhat-highcontrast-dark': redhatHighcontrastDarkTokens
+  }}/>
   </React.Fragment>
 );
 Component.displayName = 'FoundationsAndStylesDesignTokensAllDesignTokensDesignTokensDocs';
